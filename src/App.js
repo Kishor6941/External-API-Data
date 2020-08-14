2c330a61f1b44cb79da6455acc25ed060fbe8bfb
Kishor6941 <phawadeks9049@gmail.com>
2020-08-14 16:04:42 +0530

    fetched external api data

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
+
+const gitHuburl = "https://api.github.com/users/kishor6941";
 
 function App() {
+  const [userData, setUserData] = useState({});
+  useEffect(() => {
+    getGiHubUserWithAxios();
+  }, []);
+
+  const getGiHubUserWithAxios = async () => {
+    const response = await axios.get(gitHuburl);
+    setUserData(response.data);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h2>GitHub User Data</h2>
+      </div>
+      <div className="user-container">
+        <h5 className="info-item">{userData.followers}</h5>
+        <h5 className="info-item">{userData.html_url}</h5>
+        <h5 className="info-item">{userData.blog}</h5>
+        <h5 className="info-item">{userData.public_repos}</h5>
+      </div>
     </div>
   );
 }
-
 export default App;
